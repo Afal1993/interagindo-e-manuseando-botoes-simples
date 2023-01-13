230288b6cbd984848e90bc5ad75e6c785d541210
Alexandre Lima <afal1993profissional@gmail.com>
2023-01-12 22:04:54 -0300

    interagindo e manuseando botões simples

---
+++ button.js
@@ -0,0 +1,34 @@
+const changeVisorValueEduardo = document.querySelector('[id="buttEduardo"]');
+      changeVisorValueEduardo.addEventListener('click', function() {
+            texto.value = 'Eduardo';
+      });
+
+const changeVisorValueEdileuza = document.querySelector('[id="buttEdileuza"]');
+      changeVisorValueEdileuza.addEventListener('click', function(){
+            texto.value = 'Edileuza';
+      });
+
+const changeVisorValueAriana = document.querySelector('[id="buttAriana"]');
+      changeVisorValueAriana.addEventListener('click', function() {
+            texto.value = 'Ariana';
+      });
+
+const changeVisorValueAlexandre = document.querySelector('[id="buttAlexandre"]');
+      changeVisorValueAlexandre.addEventListener('click', function() {
+            texto.value = 'Alexandre';
+      })
+
+const changeVisorValueAlan = document.querySelector('[id="buttAlan');
+      changeVisorValueAlan.addEventListener('click', function() {
+            texto.value = 'Alan';
+      });
+
+const changeVisorValueJonatha = document.querySelector('[id="buttJonatha"]');
+      changeVisorValueJonatha.addEventListener('click', function() {
+            texto.value = 'Jonatha';
+      });
+
+const buttClear = document.querySelector('[id="buttClear"]');
+      buttClear.addEventListener('click', function() {
+            texto.value = '';
+      })
