97116f7dd89a440edf0d7d8e2a444aef8acb821f
Alexandre Lima <afal1993profissional@gmail.com>
2023-01-13 18:52:45 -0300

    brincando com imagens do Tibia

---
+++ tibia.js
@@ -0,0 +1,36 @@
+const creatureNameAmazon = document.querySelector('[id="c-1"]');
+      creatureNameAmazon.addEventListener('click', function() {
+        return visor.value = 'Amazon';
+      });
+const creatureNamebehemoth = document.querySelector('[id="c-2"]');
+      creatureNamebehemoth.addEventListener('click', function() {
+        return visor.value = 'Behemoth';
+      });
+const creatureNameCyclops = document.querySelector('[id="c-3"]');
+      creatureNameCyclops.addEventListener('click', function() {
+        return visor.value = "Cyclops";
+      });
+const creatureNameDragon = document.querySelector('[id="c-4"]');
+      creatureNameDragon.addEventListener('click', function() {
+        return visor.value = "Dragon";
+      });
+const creatureNameElderWyrm = document.querySelector('[id="c-5"]');
+      creatureNameElderWyrm.addEventListener('click', function() {
+        return visor.value = 'Elder Wyrm';
+      });
+const creatureNameFerumbras = document.querySelector('[id="c-6"]');
+      creatureNameFerumbras.addEventListener('click', function() {
+        return visor.value = 'Ferumbras';
+      });
+const creatureNameGhoul = document.querySelector('[id="c-7"]');
+      creatureNameGhoul.addEventListener('click', function(){
+        return visor.value = "Ghoul";
+      });
+const creatureNameHero = document.querySelector('[id="c-8"]');
+      creatureNameHero.addEventListener('click', function() {
+        return visor.value = "Hero";
+      });
+const creatureNameInfernalist = document.querySelector('[id="c-9"]');
+      creatureNameInfernalist.addEventListener('click', function() {
+        return visor.value = "Infernalist";
+      });
